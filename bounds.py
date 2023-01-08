8d1051d872b94169854716898aa832cbf87b195d
taKana671 <kanae5321@gmail.com>
2023-01-08 16:59:20 +0900

    First commit

---
+++ bounds.py
@@ -0,0 +1,58 @@
+from panda3d.core import Vec3, Vec2
+
+import numpy as np
+
+
+class Bounds:
+
+    def __init__(self, vertices):
+        """vertices: list of Vec3
+        """
+        self.top_right = self.get_top_right(vertices)
+        self.bottom_left = self.get_bottom_left(vertices)
+        self.height = self.top_right.z - self.bottom_left.z
+        self.width = self.top_right.x - self.bottom_left.x
+        self.center = self.get_center(vertices)
+        self.radius = max(rad for rad in self.get_radius(vertices))
+
+    def get_top_right(self, vertices):
+        x = max(vertices, key=lambda vertex: vertex.x).x
+        y = min(vertices, key=lambda vertex: vertex.y).y
+        z = max(vertices, key=lambda vertex: vertex.z).z
+
+        return Vec3(x, y, z)
+
+    def get_bottom_left(self, vertices):
+        x = min(vertices, key=lambda vertex: vertex.x).x
+        y = max(vertices, key=lambda vertex: vertex.y).y
+        z = min(vertices, key=lambda vertex: vertex.z).z
+
+        return Vec3(x, y, z)
+
+    def get_center(self, vertices):
+        cnt = len(vertices)
+        x = sum(vertex.x for vertex in vertices) / cnt
+        y = sum(vertex.y for vertex in vertices) / cnt
+        z = sum(vertex.z for vertex in vertices) / cnt
+
+        return Vec3(x, y, z)
+
+    def get_radius(self, vertices):
+        for vertex in vertices:
+            x = vertex.x - self.center.x
+            y = vertex.y - self.center.y
+            z = vertex.z - self.center.z
+            radius = (x ** 2 + y ** 2 + z ** 2) ** 0.5
+            yield radius
+
+    def calc_uv(self, vertex):
+        """
+        vertex: Vec3
+        """
+        nm = (vertex - self.center) / self.radius
+        phi = np.arctan2(nm.z, nm.x)
+        theta = np.arcsin(nm.y)
+        u = (phi + np.pi) / (2 * np.pi)
+        v = (theta + np.pi / 2) / np.pi
+
+        return Vec2(u, v)
